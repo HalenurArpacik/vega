8075b6fee165c080b8d4a334201ad2653f11352d
Halenur Arpacık <halenur.arpacik@gmail.com>
2025-07-10 12:14:27 +0300

    Initial Commit

---
+++ exampleProject/exampleProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  exampleProjectApp.swift
+//  exampleProject
+//
+//  Created by Halenur Arpacık on 10.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct exampleProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
